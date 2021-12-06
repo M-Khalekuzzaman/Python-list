bdd1f5ccfcbc9fca3233a36af15bbdd0d4537688
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-12-06 12:46:23 +0600

    Add files via upload

---
+++ list(1).py
@@ -0,0 +1,14 @@
+
+#List is a libary function of python programming language:
+
+subject = ["C","C++","Python","Java","Javascript","Bootsrap","Jquery"]
+print(subject)
+print(subject[1])
+print(subject[2:])
+print(subject[-1])
+print(subject + ["html","CSs"])
+
+#using in and not in
+
+print("html" not in subject)
+
